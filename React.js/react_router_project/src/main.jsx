d4e7022ce89626af124aec41dcef18c495b762ca
Arshad Faraz <arshadfarazusmani@gmail.com>
2025-08-28 09:22:30 -0700

    add react router doc and project to code base

---
+++ React.js/react_router_project/src/main.jsx
@@ -0,0 +1,64 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/home/home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/contact/contact.jsx'
+import User from './components/user/user.jsx'
+import CatalogPage from './components/catalauge/catalauge.jsx'
+import CartPage from './components/cart/cart.jsx'
+
+const router= createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout/>,
+    children : [
+
+      {
+        path:"",
+        element: <Home/>
+      },
+
+      {
+        path:"/About",
+        element: <About/>
+      },
+      {
+        path:"/Contact",
+        element: <Contact/>
+      },
+
+      {
+        path:"/user/:id",
+        element: <User/>
+      
+      },
+      {
+        path:"/catalog",
+        element: <CatalogPage/>
+      
+      },
+      {
+        path:"/cart",
+        element: <CartPage/>
+      
+      }
+      
+      
+    ]
+
+    
+
+
+  }
+])
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router}/>
+  </StrictMode>,
+)
